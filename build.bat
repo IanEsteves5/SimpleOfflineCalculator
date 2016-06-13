904a91d7a04da0b0fc3717bd273c498031858dcd
Ian Esteves do Nascimento <ianesteves5@gmail.com>
2016-06-13 06:57:08 -0300

    Changed project organization

--- build.bat
+++ build.bat
@@ -32,16 +32,16 @@ xcopy _locales %BUILD_ROOT%\_locales /s /i /f
 
 mkdir %BUILD_ROOT%\resources
 mkdir %BUILD_ROOT%\resources\css
-java -jar %YUI_COMPRESSOR_JAR% resources\css\common.css -o %BUILD_ROOT%\resources\css\common.min.css
-java -jar %YUI_COMPRESSOR_JAR% resources\css\popup.css -o %BUILD_ROOT%\resources\css\popup.min.css
-java -jar %YUI_COMPRESSOR_JAR% resources\css\tree.css -o %BUILD_ROOT%\resources\css\tree.min.css
-java -jar %YUI_COMPRESSOR_JAR% resources\css\plot.css -o %BUILD_ROOT%\resources\css\plot.min.css
+java -jar %YUI_COMPRESSOR_JAR% resources\css\common.css -o %BUILD_ROOT%\resources\css\common.css
+java -jar %YUI_COMPRESSOR_JAR% resources\css\popup.css -o %BUILD_ROOT%\resources\css\popup.css
+java -jar %YUI_COMPRESSOR_JAR% resources\css\tree.css -o %BUILD_ROOT%\resources\css\tree.css
+java -jar %YUI_COMPRESSOR_JAR% resources\css\plot.css -o %BUILD_ROOT%\resources\css\plot.css
 
 mkdir %BUILD_ROOT%\resources\javascript
-java -jar %YUI_COMPRESSOR_JAR% resources\javascript\calculator.js -o %BUILD_ROOT%\resources\javascript\calculator.min.js
-java -jar %YUI_COMPRESSOR_JAR% resources\javascript\popup.js -o %BUILD_ROOT%\resources\javascript\popup.min.js
-java -jar %YUI_COMPRESSOR_JAR% resources\javascript\tree.js -o %BUILD_ROOT%\resources\javascript\tree.min.js
-java -jar %YUI_COMPRESSOR_JAR% resources\javascript\plot.js -o %BUILD_ROOT%\resources\javascript\plot.min.js
+java -jar %YUI_COMPRESSOR_JAR% resources\javascript\calculator.js -o %BUILD_ROOT%\resources\javascript\calculator.js
+java -jar %YUI_COMPRESSOR_JAR% resources\javascript\popup.js -o %BUILD_ROOT%\resources\javascript\popup.js
+java -jar %YUI_COMPRESSOR_JAR% resources\javascript\tree.js -o %BUILD_ROOT%\resources\javascript\tree.js
+java -jar %YUI_COMPRESSOR_JAR% resources\javascript\plot.js -o %BUILD_ROOT%\resources\javascript\plot.js
 
 mkdir %BUILD_ROOT%\resources\image
 %INKSCAPE_EXE% -f resources\image\icon.svg -w 16 -h 16 -e %BUILD_ROOT%\resources\image\icon_16.png
